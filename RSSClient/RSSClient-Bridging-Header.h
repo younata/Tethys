fa7ad472b9b6ad38dfdeb553a77bdd9c32535054
Rachel Brindle <you@subluminal.net>
2014-10-04 08:34:50 -0700

    minor changes. I'm busy today.

--- RSSClient/RSSClient-Bridging-Header.h
+++ RSSClient/RSSClient-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <AFNetworking/AFNetworking.h>
+#import "AFNetworking.h"
 #import "PureLayout.h"
 #import "RSBDragMenu.h"
 #import "MWFeedParser.h"

bee382593df2e5747658f20bdc1fe161815e32ed
Rachel Brindle <you@subluminal.net>
2014-10-18 16:12:19 -0700

    added checks to make sure that background fetch is enabled.

--- RSSClient/AppDelegate.swift
+++ RSSClient/AppDelegate.swift
@@ -34,6 +34,15 @@ public class AppDelegate: UIResponder, UIApplicationDelegate {
                 }
             }
         }
+        
+        dispatch_async(DISPATCH_QUEUE_PRIORITY_BACKGROUND) {
+            if DataManager.sharedInstance().feeds().count > 0 {
+                application.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
+            } else {
+                application.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalNever)
+            }
+        }
+        
         return true
     }
     

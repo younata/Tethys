42aefc4451486304cc503a3dba8439ab4ccccf89
Rachel Brindle <you@subluminal.net>
2015-01-09 20:28:17 -0800

    Addition of function to estimate when a feed will next update. Not used.

--- RSSClient/DataManager.swift
+++ RSSClient/DataManager.swift
@@ -386,6 +386,38 @@ class DataManager: NSObject {
         }
     }
     
+    func estimateNextFeedTime(feed: Feed) -> (NSDate?, Double) { // Time, stddev
+        // This could be much better done.
+        // For example, some feeds only update on weekdays, which this would tell it to update
+        // Once every 7/5ths of a day, instead of once a day for 5 days, then not at all on the weekends.
+        // But for now, it's ok.
+        let times : [NSTimeInterval] = feed.allArticles(self).map {
+            return $0.published.timeIntervalSince1970
+        }
+        if times.count < 2 {
+            return (nil, 0)
+        }
+        
+        func mean(values: [Double]) -> Double {
+            return (values.reduce(0.0) { return $0 + $1 }) / Double(values.count)
+        }
+        
+        var intervals : [NSTimeInterval] = []
+        for (i, t) in enumerate(times) {
+            if i == (times.count - 1) {
+                break
+            }
+            intervals.append(times[i+1] - t)
+        }
+        let averageTimeInterval = mean(intervals)
+        
+        func stdev(values: [Double], average: Double) -> Double {
+            return sqrt(mean(values.map { pow($0 - average, 2) }))
+        }
+        
+        return (NSDate(timeIntervalSince1970: times.last! + averageTimeInterval), stdev(intervals, averageTimeInterval))
+    }
+    
     func setApplicationBadgeCount() {
         let num = feeds(managedObjectContext: managedObjectContext).filter {return !$0.isQueryFeed()}.reduce(0) {return $0 + Int($1.unreadArticles(self))}
         #if os(iOS)

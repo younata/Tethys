3cdcd0d2d3b9cef4a0172a72d526aa181d2f4598
Rachel Brindle <you@subluminal.net>
2015-02-25 23:02:03 -0800

    Tests performance, sorta

--- RSSClientTests/FeedParserSpec.swift
+++ RSSClientTests/FeedParserSpec.swift
@@ -1,20 +1,42 @@
 import Quick
 import Nimble
-import XCTest
 
 class FeedParserSpec: QuickSpec {
     override func spec() {
-        let bundle = NSBundle(forClass: OPMLParserSpec.self)
-        let str = String(contentsOfFile: bundle.pathForResource("test", ofType: "opml")!, encoding: NSUTF8StringEncoding, error: nil)!
+        let bundle = NSBundle(forClass: FeedParserSpec.self)
+        let str = String(contentsOfFile: bundle.pathForResource("feed", ofType: "rss")!, encoding: NSUTF8StringEncoding, error: nil)!
+
+        var parsers : [FeedParser] = []
 
         describe("performance tests") {
             it("is reasonably performant") {
-//                self.measureBlock() {
-//                    for i in 0..<1000 {
-//                        let fp = FeedParser(string: str)
-//                        fp.parse()
-//                    }
-//                }
+                let startTime = CACurrentMediaTime();
+                let amount = 20
+                println("\n\n\n\n\n")
+                for i in 0..<amount {
+                    let fp = FeedParser(string: str)
+                    fp.asynch = true
+                    fp.success {(_, _) in
+                        parsers.removeAtIndex(0)
+                        if parsers.count == 0 {
+                            let time = (CACurrentMediaTime() - startTime) / Double(amount);
+                            println("1 run takes about \(time) seconds")
+                            println("\n\n\n\n\n")
+                        }
+                    }
+                    fp.failure {(error) in
+                        println("\(error)")
+                        parsers.removeAtIndex(0)
+                        if parsers.count == 0 {
+                            let time = (CACurrentMediaTime() - startTime) / Double(amount);
+                            println("1 run takes about \(time) seconds")
+                            println("\n\n\n\n\n")
+                        }
+                    }
+                    parsers.append(fp)
+                    fp.parse()
+                }
+                expect(parsers.count).toEventually(equal(0), timeout: 60)
             }
         }
     }

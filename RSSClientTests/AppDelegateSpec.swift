82f9bba3bfef55edcd9ee0460e205c85c32baa76
Rachel Brindle <you@subluminal.net>
2015-08-15 09:33:46 -0700

    Show the network activity indicator when we update feeds

--- RSSClientTests/AppDelegateSpec.swift
+++ RSSClientTests/AppDelegateSpec.swift
@@ -61,7 +61,6 @@ class AppDelegateSpec: QuickSpec {
 
             subject.anInjector = injector
             subject.window = UIWindow(frame: CGRectMake(0, 0, 320, 480))
-            // Apparently, calling "-makeKeyAndVisible" on a window in test will cause a crash.
         }
         
         describe("-application:didFinishLaunchingWithOptions:") {
---
+++ RSSClientTests/UIApplication+rNewsSpec.swift
@@ -0,0 +1,69 @@
+import Quick
+import Nimble
+import rNews
+import rNewsKit
+
+class UIApplicationRNewsSpec: QuickSpec {
+    override func spec() {
+        var subject: UIApplication! = nil
+        var originalBadgeCount: Int = 0
+
+        beforeEach {
+            subject = UIApplication.sharedApplication()
+            originalBadgeCount = subject.applicationIconBadgeNumber
+            subject.applicationIconBadgeNumber = 1
+        }
+
+        afterEach {
+            subject.applicationIconBadgeNumber = originalBadgeCount
+        }
+
+        it("increments the badge number when an article is marked as unread") {
+            let article = Article(title: "", link: nil, summary: "", author: "", published: NSDate(), updatedAt: nil, identifier: "", content: "", read: true, feed: nil, flags: [], enclosures: [])
+            subject.markedArticle(article, asRead: false)
+
+            expect(subject.applicationIconBadgeNumber).to(equal(2))
+        }
+
+        it("decrements the badge number when an article is marked as read") {
+            let article = Article(title: "", link: nil, summary: "", author: "", published: NSDate(), updatedAt: nil, identifier: "", content: "", read: true, feed: nil, flags: [], enclosures: [])
+            subject.markedArticle(article, asRead: true)
+
+            expect(subject.applicationIconBadgeNumber).to(equal(0))
+        }
+
+        it("decrements the badge number if an unread article is deleted") {
+            let article = Article(title: "", link: nil, summary: "", author: "", published: NSDate(), updatedAt: nil, identifier: "", content: "", read: false, feed: nil, flags: [], enclosures: [])
+            subject.deletedArticle(article)
+
+            expect(subject.applicationIconBadgeNumber).to(equal(0))
+        }
+
+        it("does nothing to the badge number if a read article is deleted") {
+            let article = Article(title: "", link: nil, summary: "", author: "", published: NSDate(), updatedAt: nil, identifier: "", content: "", read: true, feed: nil, flags: [], enclosures: [])
+            subject.deletedArticle(article)
+
+            expect(subject.applicationIconBadgeNumber).to(equal(1))
+        }
+
+        describe("On update feed start") {
+            beforeEach {
+                subject.willUpdateFeeds()
+            }
+
+            it("shows the network activity indicator") {
+                expect(subject.networkActivityIndicatorVisible).to(beTruthy())
+            }
+
+            describe("when we stop updating feeds") {
+                beforeEach {
+                    subject.didUpdateFeeds([])
+                }
+
+                it("stops showing the network activity indicator") {
+                    expect(subject.networkActivityIndicatorVisible).to(beFalsy())
+                }
+            }
+        }
+    }
+}

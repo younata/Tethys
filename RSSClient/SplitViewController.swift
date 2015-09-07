b221e3e0d04e5548f796585a0f5dcd328fcd86ac
Rachel Brindle <you@subluminal.net>
2015-09-07 10:33:01 -0700

    Make the splitviewcontroller behave better

--- RSSClient/SplitViewController.swift
+++ RSSClient/SplitViewController.swift
@@ -3,19 +3,27 @@ import UIKit
 public class SplitViewController: UISplitViewController {
     public var collapseDetailViewController: Bool = true
 
-    public override var delegate: UISplitViewControllerDelegate? {
-        get {
-            return self
-        }
-        set {}
-    }
-
-    public lazy var themeRepository: ThemeRepository? = {
+    private lazy var themeRepository: ThemeRepository? = {
         return self.injector?.create(ThemeRepository.self) as? ThemeRepository
     }()
 
     public override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return self.themeRepository?.theme == .Dark ? .LightContent : super.preferredStatusBarStyle()
+    }
+
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+
+        self.delegate = self
+        self.preferredDisplayMode = .PrimaryOverlay
+    }
+
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError()
     }
 }
 
@@ -30,5 +38,13 @@ extension SplitViewController: UISplitViewControllerDelegate {
         collapseSecondaryViewController secondaryViewController: UIViewController,
         ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
             return self.collapseDetailViewController
+    }
+
+    public func primaryViewControllerForCollapsingSplitViewController(splitViewController: UISplitViewController) -> UIViewController? {
+        return self.viewControllers.first
+    }
+
+    public func targetDisplayModeForActionInSplitViewController(svc: UISplitViewController) -> UISplitViewControllerDisplayMode {
+        return .AllVisible
     }
 }

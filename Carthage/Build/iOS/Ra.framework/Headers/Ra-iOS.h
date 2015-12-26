6a6c21f49ce93d06128838533d003c984bced21b
Rachel Brindle <you@subluminal.net>
2015-12-25 22:53:34 -0800

    Add Realm as a dependency, update iOS dependencies

---
+++ Carthage/Build/iOS/Ra.framework/Headers/Ra-iOS.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ra-iOS.
+FOUNDATION_EXPORT double Ra_iOSVersionNumber;
+
+//! Project version string for Ra-iOS.
+FOUNDATION_EXPORT const unsigned char Ra_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ra_iOS/PublicHeader.h>

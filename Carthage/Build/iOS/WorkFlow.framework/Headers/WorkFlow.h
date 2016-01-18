aa643bd8732223be574a5ca950c6b9222ee5e433
Rachel Brindle <you@subluminal.net>
2016-01-17 19:05:46 -0800

    Add WorkFlow as a dependency

---
+++ Carthage/Build/iOS/WorkFlow.framework/Headers/WorkFlow.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for WorkFlow.
+FOUNDATION_EXPORT double WorkFlowVersionNumber;
+
+//! Project version string for WorkFlow.
+FOUNDATION_EXPORT const unsigned char WorkFlowVersionString[];

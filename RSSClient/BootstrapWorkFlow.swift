acdc9bbf0641610f909ef4f58abe45acb38638e7
Rachel Brindle <you@subluminal.net>
2016-02-24 17:45:55 -0800

    BootstrapWorkFlow can also handle cases where we have articles to open up

--- RSSClient/BootstrapWorkFlow.swift
+++ RSSClient/BootstrapWorkFlow.swift
@@ -10,20 +10,25 @@ public class BootstrapWorkFlow {
     private let migrationUseCase: MigrationUseCase
     private let splitViewController: SplitViewController
     private let feedsTableViewController: Void -> FeedsTableViewController
+    private let articleViewController: Void -> ArticleViewController
 
     private let initialLaunch = false
 
+    // swiftlint:disable function_parameter_count
     public init(window: UIWindow,
         feedRepository: FeedRepository,
         migrationUseCase: MigrationUseCase,
         splitViewController: SplitViewController,
-        feedsTableViewController: Void -> FeedsTableViewController) {
+        feedsTableViewController: Void -> FeedsTableViewController,
+        articleViewController: Void -> ArticleViewController) {
         self.window = window
         self.feedRepository = feedRepository
         self.migrationUseCase = migrationUseCase
         self.splitViewController = splitViewController
         self.feedsTableViewController = feedsTableViewController
+        self.articleViewController = articleViewController
     }
+    // swiftlint:enable function_parameter_count
 
     public convenience init(window: UIWindow, injector: Injector) {
         self.init(
@@ -31,11 +36,14 @@ public class BootstrapWorkFlow {
             feedRepository: injector.create(FeedRepository)!,
             migrationUseCase: injector.create(MigrationUseCase)!,
             splitViewController: injector.create(SplitViewController)!,
-            feedsTableViewController: { injector.create(FeedsTableViewController)! }
+            feedsTableViewController: { injector.create(FeedsTableViewController)! },
+            articleViewController: { injector.create(ArticleViewController)! }
         )
     }
 
-    public func begin() {
+    private var feedAndArticle: (feed: Feed, article: Article)?
+    public func begin(feedAndArticle: (feed: Feed, article: Article)? = nil) {
+        self.feedAndArticle = feedAndArticle
         if self.initialLaunch {
 
         } else {
@@ -61,13 +69,23 @@ public class BootstrapWorkFlow {
     }
 
     private func showFeedsController() {
+        let feedsTableViewController = self.feedsTableViewController()
         self.splitViewController.masterNavigationController.viewControllers = [
-            self.feedsTableViewController()
+            feedsTableViewController
+        ]
+        self.splitViewController.detailNavigationController.viewControllers = [
+            self.articleViewController()
         ]
         self.splitViewController.viewControllers = [
             self.splitViewController.masterNavigationController,
             self.splitViewController.detailNavigationController
         ]
+
+        if let (feed, article) = self.feedAndArticle {
+            let articleListController = feedsTableViewController.showFeeds([feed], animated: false)
+            articleListController.showArticle(article, animated: false)
+        }
+
         self.window.rootViewController = self.splitViewController
     }
 }
--- RSSClientTests/BootstrapWorkFlowSpec.swift
+++ RSSClientTests/BootstrapWorkFlowSpec.swift
@@ -25,7 +25,7 @@ class BootstrapWorkFlowSpec: QuickSpec {
             subject = BootstrapWorkFlow(window: window, injector: injector)
         }
 
-        sharedExamples("showing the feeds list") {
+        sharedExamples("showing the feeds list") { (sharedContext: SharedExampleContext) in
             var splitViewController: UISplitViewController?
 
             beforeEach {
@@ -39,20 +39,57 @@ class BootstrapWorkFlowSpec: QuickSpec {
                 }
             }
 
-            describe("master view controller") {
+            describe("the master view controller") {
                 var vc: UIViewController?
 
                 beforeEach {
                     vc = splitViewController?.viewControllers.first
                 }
 
-                it("should be an instance of UINavigationController") {
+                it("is an instance of UINavigationController") {
                     expect(vc).to(beAnInstanceOf(UINavigationController.self))
                 }
 
-                it("should have a FeedsTableViewController as the root controller") {
+                it("has a FeedsTableViewController as the root controller") {
                     let nc = vc as? UINavigationController
                     expect(nc?.viewControllers.first).to(beAnInstanceOf(FeedsTableViewController.self))
+                }
+
+                it("shows a feeds list if a feed was specified") {
+                    guard let feed = sharedContext()["feed"] as? Feed else {
+                        return
+                    }
+                    let nc = vc as? UINavigationController
+                    expect(nc?.topViewController).to(beAnInstanceOf(ArticleListController.self))
+                    guard let articleListController = nc?.topViewController as? ArticleListController else { return }
+                    expect(articleListController.feeds) == [feed]
+                }
+            }
+
+            describe("the detail view controller") {
+                var vc: UIViewController?
+
+                beforeEach {
+                    vc = splitViewController?.viewControllers.last
+                }
+
+                it("is an instance of UINavigationController") {
+                    expect(vc).to(beAnInstanceOf(UINavigationController.self))
+                }
+
+                it("has an ArticleViewController as the root controller") {
+                    let nc = vc as? UINavigationController
+                    expect(nc?.viewControllers.first).to(beAnInstanceOf(ArticleViewController.self))
+                }
+
+                it("shows an article if an article was specified") {
+                    guard let article = sharedContext()["article"] as? Article else {
+                        return
+                    }
+                    let nc = vc as? UINavigationController
+                    expect(nc?.topViewController).to(beAnInstanceOf(ArticleViewController.self))
+                    guard let articleViewController = nc?.topViewController as? ArticleViewController else { return }
+                    expect(articleViewController.article) == article
                 }
             }
         }
@@ -90,6 +127,45 @@ class BootstrapWorkFlowSpec: QuickSpec {
                 }
 
                 itBehavesLike("showing the feeds list")
+            }
+
+            describe("when provided with an article and a feed") {
+                let feed = Feed(title: "", url: nil, summary: "", query: nil, tags: [], waitPeriod: 0, remainingWait: 0, articles: [], image: nil)
+                let article = Article(title: "", link: nil, summary: "", author: "", published: NSDate(), updatedAt: nil, identifier: "", content: "", read: false, estimatedReadingTime: 0, feed: nil, flags: [], enclosures: [])
+
+                context("if a migration is available") {
+                    beforeEach {
+                        feedRepository._databaseUpdateAvailable = true
+
+                        subject.begin((feed, article))
+                    }
+
+                    it("begins the migration use case") {
+                        expect(migrationUseCase.beginWorkCallCount) == 1
+                    }
+
+                    describe("when the migration finishes") {
+                        beforeEach {
+                            migrationUseCase.beginWorkArgsForCall(0)()
+                        }
+
+                        itBehavesLike("showing the feeds list") {
+                            ["feed": feed, "article": article]
+                        }
+                    }
+                }
+
+                context("otherwise") {
+                    beforeEach {
+                        feedRepository._databaseUpdateAvailable = false
+
+                        subject.begin((feed, article))
+                    }
+
+                    itBehavesLike("showing the feeds list") {
+                        ["feed": feed, "article": article]
+                    }
+                }
             }
         }
     }

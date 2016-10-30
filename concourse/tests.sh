b14fa5cdd07761caeef7227ed93f4c194eb35f01
Rachel Brindle <you@subluminal.net>
2016-10-30 10:44:51 -0700

    inline a conditional

--- concourse/tests.sh
+++ concourse/tests.sh
@@ -2,11 +2,6 @@
 
 cd rssclient
 bundle
-bundle exec carthage_cache -b $AWS_CACHE_BUCKET install
-if [ $? != 0 ]; then
-    echo $?
-    carthage bootstrap --platform ios,mac
-    bundle exec carthage_cache -b $AWS_CACHE_BUCKET publish
-fi
+bundle exec carthage_cache -b $AWS_CACHE_BUCKET install || (carthage bootstrap --platform ios,mac; bundle exec carthage_cache -b $AWS_CACHE_BUCKET publish)
 carthage bootstrap --platform ios
 bundle exec fastlane test

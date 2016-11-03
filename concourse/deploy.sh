6306333f517614fcbf55414f92eb6cfa7281d0ed
Rachel Brindle <you@subluminal.net>
2016-11-03 13:04:59 -0700

    Have CI create libraries documentation before running tests/deploying

--- concourse/deploy.sh
+++ concourse/deploy.sh
@@ -3,6 +3,7 @@
 cd rssclient
 bundle
 bundle exec carthage_cache -b $AWS_CACHE_BUCKET install || carthage bootstrap --no-use-binaries --platform ios
+bundle exec documentation:libraries
 echo "$MIXPANEL_TOKEN" > .mixpanel
 echo "$PASIPHAE_TOKEN" > .pasiphae
 echo "$PASIPHAE_URL" > .pasiphaeURL
--- concourse/tests.sh
+++ concourse/tests.sh
@@ -3,4 +3,5 @@
 cd rssclient
 bundle
 bundle exec carthage_cache -b $AWS_CACHE_BUCKET install || (carthage bootstrap --platform ios,mac --no-use-binaries; bundle exec carthage_cache -b $AWS_CACHE_BUCKET publish)
+bundle exec documentation:libraries
 bundle exec fastlane test

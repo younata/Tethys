1c92f9549750c7389b9e9ab2bdba9b622964d40f
Rachel Brindle <you@subluminal.net>
2016-11-16 21:55:29 -0800

    Have concourse ensure we're on latest carthage dependencies

--- concourse/tests.sh
+++ concourse/tests.sh
@@ -2,6 +2,7 @@
 
 cd tethys_github
 bundle
+carthage update --no-build --no-use-binaries
 bundle exec carthage_cache -b $AWS_CACHE_BUCKET install || (carthage bootstrap --platform ios,mac --no-use-binaries; bundle exec carthage_cache -b $AWS_CACHE_BUCKET publish)
 bundle exec rake documentation:libraries
 bundle exec fastlane test

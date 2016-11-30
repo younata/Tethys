79ae263cd422c63d23081146f3921a0fe735d8e0
Rachel Brindle <you@subluminal.net>
2016-11-29 19:17:46 -0800

    Add an action to be able to deploy to external testers

--- concourse/deploy.sh
+++ concourse/deploy.sh
@@ -8,9 +8,15 @@ echo "$MIXPANEL_TOKEN" > .mixpanel
 echo "$PASIPHAE_TOKEN" > .pasiphae
 echo "$PASIPHAE_URL" > .pasiphaeURL
 
-if [[ `git name-rev --name-only --tags HEAD` = 'undefined' ]]
+CURRENT_TAG=`git name-rev --name-only --tags HEAD`
+shopt -s nocasematch
+
+if [[ "$CURRENT_TAG" = 'undefined' ]]
 then
     bundle exec fastlane deploy_testflight
+elif [[ "$CURRENT_TAG" =~ [RC\d$] ]]
+then
+    bundle exec fastlane deploy_testflight_external
 else
     bundle exec fastlane deploy_app_store
 fi

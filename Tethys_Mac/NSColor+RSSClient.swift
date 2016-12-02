225a2867e73e3c8f033e3d670a45a55689515c54
Rachel Brindle <you@subluminal.net>
2016-12-02 09:39:28 -0800

    Add Sponde and such to the mac version

--- Tethys_Mac/NSColor+RSSClient.swift
+++ Tethys_Mac/NSColor+RSSClient.swift
@@ -2,6 +2,10 @@ import Cocoa
 
 extension NSColor {
     class var darkGreen: NSColor {
-        return NSColor(calibratedRed: 0, green: 0.5, blue: 0, alpha: 1.0)
+        return NSColor(calibratedRed: 0, green: 0.75, blue: 0, alpha: 1.0)
+    }
+
+    class var darkVioletColor: NSColor {
+        return NSColor(calibratedRed: 0.25, green: 0, blue: 0.5, alpha: 1.0)
     }
 }

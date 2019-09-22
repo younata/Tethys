631fab174d4ea0306e4ca0aece4017b5c3ded0d0
Rachel Brindle <you@subluminal.net>
2019-09-21 21:20:44 -0700

    Add asset for black icon
    [#168649352]

--- assets/generate_icons.sh
+++ assets/generate_icons.sh
@@ -4,8 +4,8 @@ export APP_NAME=Tethys
 
 function settings_icon {
     parallel --progress -j 0 << EOF
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/settings.imageset/settings@2x.png 48:48
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/settings.imageset/settings@3x.png 72:72
+svgexport AppIcon.xml ../$APP_NAME/Assets.xcassets/settings.imageset/settings@2x.png 48:48
+svgexport AppIcon.xml ../$APP_NAME/Assets.xcassets/settings.imageset/settings@3x.png 72:72
 EOF
 }
 
@@ -16,24 +16,24 @@ svgexport Chevron.xml Chevron@3x.png 72:72
 EOF
 
     parallel --progress -j 0 << EOF
-convert Chevron@2x.png -rotate -90 ../$APP_NAME/Images.xcassets/LeftChevron.imageset/LeftChevron@2x.png
-convert Chevron@3x.png -rotate -90 ../$APP_NAME/Images.xcassets/LeftChevron.imageset/LeftChevron@3x.png
-convert Chevron@2x.png -rotate 90 ../$APP_NAME/Images.xcassets/RightChevron.imageset/RightChevron@2x.png
-convert Chevron@3x.png -rotate 90 ../$APP_NAME/Images.xcassets/RightChevron.imageset/RightChevron@3x.png
+convert Chevron@2x.png -rotate -90 ../$APP_NAME/Assets.xcassets/LeftChevron.imageset/LeftChevron@2x.png
+convert Chevron@3x.png -rotate -90 ../$APP_NAME/Assets.xcassets/LeftChevron.imageset/LeftChevron@3x.png
+convert Chevron@2x.png -rotate 90 ../$APP_NAME/Assets.xcassets/RightChevron.imageset/RightChevron@2x.png
+convert Chevron@3x.png -rotate 90 ../$APP_NAME/Assets.xcassets/RightChevron.imageset/RightChevron@3x.png
 EOF
     rm Chevron@2x.png Chevron@3x.png
 }
 
 function app_icon {
+    export ASSET_PATH="../$APP_NAME/Assets.xcassets/AppIcon.appiconset"
     parallel --progress -j 0 << EOF
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/AppIcon.appiconset/Icon@2x.png 120:120
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/AppIcon.appiconset/Icon@3x.png 180:180
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/AppIcon.appiconset/Icon-iPadPro@2x.png 167:167
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/AppIcon.appiconset/Icon-iPad.png 76:76
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/AppIcon.appiconset/Icon-iPad@2x.png 152:152
-svgexport AppIcon.xml ../$APP_NAME/Images.xcassets/AppIcon.appiconset/Icon-AppStore.png 1024:1024
+svgexport AppIcon.xml $ASSET_PATH/Icon@2x.png 120:120
+svgexport AppIcon.xml $ASSET_PATH/Icon@3x.png 180:180
+svgexport AppIcon.xml $ASSET_PATH/Icon-iPadPro@2x.png 167:167
+svgexport AppIcon.xml $ASSET_PATH/Icon-iPad.png 76:76
+svgexport AppIcon.xml $ASSET_PATH/Icon-iPad@2x.png 152:152
+svgexport AppIcon.xml $ASSET_PATH/Icon-AppStore.png 1024:1024
 EOF
-    export ASSET_PATH="../$APP_NAME/Images.xcassets/AppIcon.appiconset"
     parallel --progress -j 0 << EOF
 convert $ASSET_PATH/Icon@2x.png -background white -alpha remove $ASSET_PATH/Icon@2x.png
 convert $ASSET_PATH/Icon@3x.png -background white -alpha remove $ASSET_PATH/Icon@3x.png
@@ -44,8 +44,28 @@ convert $ASSET_PATH/Icon-AppStore.png -background white -alpha remove $ASSET_PAT
 EOF
 }
 
+function app_icon_black {
+    export ASSET_PATH="../$APP_NAME/Assets.xcassets/AppIcon-Black.appiconset"
+    parallel --progress -j 0 << EOF
+svgexport AppIcon.xml $ASSET_PATH/AppIcon-Black@2x.png 120:120
+svgexport AppIcon.xml $ASSET_PATH/AppIcon-Black@3x.png 180:180
+svgexport AppIcon.xml $ASSET_PATH/AppIcon-Black-iPadPro@2x.png 167:167
+svgexport AppIcon.xml $ASSET_PATH/AppIcon-Black-iPad.png 76:76
+svgexport AppIcon.xml $ASSET_PATH/AppIcon-Black-iPad@2x.png 152:152
+svgexport AppIcon.xml $ASSET_PATH/AppIcon-Black-AppStore.png 1024:1024
+EOF
+    parallel --progress -j 0 << EOF
+convert $ASSET_PATH/AppIcon-Black@2x.png -background black -alpha remove $ASSET_PATH/AppIcon-Black@2x.png
+convert $ASSET_PATH/AppIcon-Black@3x.png -background black -alpha remove $ASSET_PATH/AppIcon-Black@3x.png
+convert $ASSET_PATH/AppIcon-Black-iPadPro@2x.png -background black -alpha remove $ASSET_PATH/AppIcon-Black-iPadPro@2x.png
+convert $ASSET_PATH/AppIcon-Black-iPad.png -background black -alpha remove $ASSET_PATH/AppIcon-Black-iPad.png
+convert $ASSET_PATH/AppIcon-Black-iPad@2x.png -background black -alpha remove $ASSET_PATH/AppIcon-Black-iPad@2x.png
+convert $ASSET_PATH/AppIcon-Black-AppStore.png -background black -alpha remove $ASSET_PATH/AppIcon-Black-AppStore.png
+EOF
+}
+
 function gray_icon {
-    export ASSET_PATH="../$APP_NAME/Images.xcassets/GrayIcon.imageset"
+    export ASSET_PATH="../$APP_NAME/Assets.xcassets/GrayIcon.imageset"
     parallel --progress -j 0 << EOF
 svgexport GrayIcon.xml $ASSET_PATH/GrayIcon@2x.png 120:120
 svgexport GrayIcon.xml $ASSET_PATH/GrayIcon@3x.png 180:180
@@ -54,18 +74,18 @@ EOF
 
 function markread_icons {
     parallel --progress -j 0 << EOF
-svgexport MarkRead.xml ../$APP_NAME/Images.xcassets/MarkRead.imageset/MarkRead.png 25:25
-svgexport MarkRead.xml ../$APP_NAME/Images.xcassets/MarkRead.imageset/MarkRead@2x.png 50:50
-svgexport MarkRead.xml ../$APP_NAME/Images.xcassets/MarkRead.imageset/MarkRead@3x.png 75:75
-svgexport MarkUnread.xml ../$APP_NAME/Images.xcassets/MarkUnread.imageset/MarkUnread.png 25:25
-svgexport MarkUnread.xml ../$APP_NAME/Images.xcassets/MarkUnread.imageset/MarkUnread@2x.png 50:50
-svgexport MarkUnread.xml ../$APP_NAME/Images.xcassets/MarkUnread.imageset/MarkUnread@3x.png 75:75
+svgexport MarkRead.xml ../$APP_NAME/Assets.xcassets/MarkRead.imageset/MarkRead.png 25:25
+svgexport MarkRead.xml ../$APP_NAME/Assets.xcassets/MarkRead.imageset/MarkRead@2x.png 50:50
+svgexport MarkRead.xml ../$APP_NAME/Assets.xcassets/MarkRead.imageset/MarkRead@3x.png 75:75
+svgexport MarkUnread.xml ../$APP_NAME/Assets.xcassets/MarkUnread.imageset/MarkUnread.png 25:25
+svgexport MarkUnread.xml ../$APP_NAME/Assets.xcassets/MarkUnread.imageset/MarkUnread@2x.png 50:50
+svgexport MarkUnread.xml ../$APP_NAME/Assets.xcassets/MarkUnread.imageset/MarkUnread@3x.png 75:75
 EOF
 }
 
 if [ $# -eq 1 ]; then
     case "$1" in
-        "app") app_icon ;;
+        "app") app_icon; app_icon_black ;;
         "chevron") chevron_icons ;;
         "gray") gray_icon ;;
         "settings") settings_icon ;;
@@ -77,6 +97,7 @@ if [ $# -eq 1 ]; then
     esac
 else
     export -f app_icon
+    export -f app_icon_black
     export -f chevron_icons
     export -f gray_icon
     export -f settings_icon
@@ -84,6 +105,7 @@ else
 
     parallel --progress -j 0 << EOF
 app_icon
+app_icon_black
 chevron_icons
 gray_icon
 settings_icon
